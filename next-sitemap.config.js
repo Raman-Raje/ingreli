d3ec574cbd7caa6e687bfbf77f1bc9bb206aaa48
Raman Shinde <raman.shinde15@gmail.com>
2025-03-13 23:17:06 +0530

    Sitemap added

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ingreli.com', // Replace with your actual site URL
+    generateRobotsTxt: true, // Generates robots.txt along with sitemap.xml
+  };
+  
