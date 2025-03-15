5354653f6fb44b3c27ba2c8b5b95de9b2ba47109
Raman Shinde <raman.shinde15@gmail.com>
2025-03-15 10:57:17 +0530

    Update BackToArticlesButton.jsx

--- src/components/Blog/BackToArticlesButton.jsx
+++ src/components/Blog/BackToArticlesButton.jsx
@@ -7,7 +7,7 @@ const BackToArticlesButton = () => {
     return (
         <div className="back-to-container">
             <Link
-                href="/blog"
+                href="/blogs"
                 className="inline-flex items-center mb-8 text-sm font-medium text-gray-600 hover:text-green-600"
         >
                 <ArrowLeft className="h-5 w-5 mr-2" /> Back to Articles
